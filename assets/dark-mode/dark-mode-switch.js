3507d6d4d82f8c10c83685bbb64dad44022a316b
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-07-31 20:47:26 +0700

    update dark mode

--- assets/dark-mode/dark-mode-switch.js
+++ assets/dark-mode/dark-mode-switch.js
@@ -7,7 +7,7 @@
         resetTheme();
       });
       function initTheme() {
-        var darkThemeSelected =
+        const darkThemeSelected =
           localStorage.getItem("darkSwitch") !== null &&
           localStorage.getItem("darkSwitch") === "dark";
         darkSwitch.checked = darkThemeSelected;
@@ -17,9 +17,19 @@
 
           if(localStorage.getItem("darkSwitch") === "dark") {
             $('.btn-primary').removeClass('btn-primary').addClass('btn-secondary');
+            setTimeout(() => {
+              $("img").each(function() {
+                let src = $(this).attr('src');
+                src = src.replace("light", "dark");
+                $(this).attr('src', src);
+            });
+            }, 300);
           }
           else {
             $('btn-secondary').removeClass('btn-secondary').addClass('btn-primary');
+            if($("img").attr('src').includes('dark')) {
+              $("img").attr('src').replace('dark', 'light');
+            }
           }
 
       }
