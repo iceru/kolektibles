9c89b8c8dd2121b27743f9f7bce3c87718de94d7
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-07-31 21:19:41 +0700

    fix mobile desktop dark

--- assets/dark-mode/dark-mode-switch.js
+++ assets/dark-mode/dark-mode-switch.js
@@ -1,22 +1,31 @@
 (function() {
   setTimeout(() => {
-    var darkSwitch = document.getElementById("darkSwitch");
+    const darkSwitch = $('.darkSwitch');
     if (darkSwitch) {
       initTheme();
-      darkSwitch.addEventListener("change", function(event) {
-        resetTheme();
+      $('.darkSwitch').change(function () { 
+          if($( window ).width() < 992) {
+            darkSwitch[0].checked = darkSwitch[1].checked;
+          } else {
+            darkSwitch[1].checked = darkSwitch[0].checked;
+          }
+
+          resetTheme();
+          initTheme();
       });
       function initTheme() {
         const darkThemeSelected =
           localStorage.getItem("darkSwitch") !== null &&
           localStorage.getItem("darkSwitch") === "dark";
-        darkSwitch.checked = darkThemeSelected;
+        darkSwitch[0].checked = darkThemeSelected;
+        darkSwitch[1].checked = darkThemeSelected;
         darkThemeSelected
           ? document.body.setAttribute("data-theme", "dark")
           : document.body.removeAttribute("data-theme");
 
           if(localStorage.getItem("darkSwitch") === "dark") {
             $('.btn-primary').removeClass('btn-primary').addClass('btn-secondary');
+            $('.darkSwitch').attr('checked', true);
             setTimeout(() => {
               $("img").each(function() {
                 let src = $(this).attr('src');
@@ -27,19 +36,27 @@
           }
           else {
             $('btn-secondary').removeClass('btn-secondary').addClass('btn-primary');
-            if($("img").attr('src').includes('dark')) {
-              $("img").attr('src').replace('dark', 'light');
-            }
+            $('.darkSwitch').attr('checked', false);
+
+            setTimeout(() => {
+              $("img").each(function() {
+                let src = $(this).attr('src');
+                src = src.replace("dark", "light");
+                $(this).attr('src', src);
+            });
+            }, 300);
           }
 
       }
       function resetTheme() {
-        if (darkSwitch.checked) {
+        if (darkSwitch[0].checked && darkSwitch[1].checked) {
           document.body.setAttribute("data-theme", "dark");
           localStorage.setItem("darkSwitch", "dark");
+          $('.darkSwitch').attr('checked', true);
         } else {
           document.body.removeAttribute("data-theme");
           localStorage.removeItem("darkSwitch");
+          $('.darkSwitch').attr('checked', false);
         }
       }
     }
